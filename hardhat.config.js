d0a3461e0159060666256643162a34c26f523afc
unknown <chiragchatur8@gmail.com>
2022-07-18 16:48:09 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/kuaJZMoosDvjaKY7L89k97wCcMyhU_Lr",
+      accounts: [ "401eba3200382cd44a2b00bfedacf8282896fea179404ff135081236f46a6012" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
